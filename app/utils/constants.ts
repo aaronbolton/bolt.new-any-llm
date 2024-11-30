9ded30745fdb851011840d3c6ccfc57c237e5232
Aaron Bolton <aaron@bukn.net>
2024-11-30 12:04:33 +0000

    Update constants.ts

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -283,11 +283,9 @@ const getOllamaBaseUrl = () => {
 };
 
 async function getOllamaModels(): Promise<ModelInfo[]> {
-  /*
-   * if (typeof window === 'undefined') {
-   *   return [];
-   * }
-   */
+  if (typeof window === 'undefined') {
+    return [];
+  }
 
   try {
     const baseUrl = getOllamaBaseUrl();
